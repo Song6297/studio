5e2747005a7f142b1fd42c6066bba152cbf5f84b
IIC Bengaluru <islamicinformationcentrebnglr@gmail.com>
2025-07-19 17:02:51 +0000

    separate those buttons from header and put  it below

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -29,8 +29,8 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center justify-between">
-        <div className="flex items-center gap-6">
+      <div className="container flex h-16 items-center">
+        <div className="flex items-center gap-6 flex-1">
             <Link href="/" aria-label="My Legal Firm Home Page" className="flex items-center gap-4">
               <Logo />
                <p className="font-headline text-sm font-bold text-primary hidden sm:block" lang="kn">ಸತ್ಯಮೇವ ಜಯತೆ</p>
@@ -86,7 +86,7 @@ export function Header() {
             </DropdownMenu>
         </div>
 
-        <div className="md:hidden">
+        <div className="md:hidden ml-auto">
           <Sheet>
             <SheetTrigger asChild>
               <Button variant="ghost" size="icon">
